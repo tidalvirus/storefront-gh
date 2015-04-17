82841a8dc16aed830c3b00405693cf5c08cbe7a5
Siraj 'Sid' Rakhada <sid-git@mindless.co.uk>
2015-04-17 13:54:31 +0800

    Start at child theme

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
+
+add_action( 'init', 'storefront_custom_logo' );
+function storefront_custom_logo() {
+	remove_action( 'storefront_header', 'storefront_site_branding', 20 );
+	add_action( 'storefront_header', 'storefront_display_custom_logo', 20 );
+}
+
+function storefront_display_custom_logo() {
+?>
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-logo-link" rel="home">
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="<?php echo get_bloginfo( 'name' ); ?>" />
+	</a>
+<?php
+}
+
